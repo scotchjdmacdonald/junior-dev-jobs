70cceb2379cd8a79fb62d9bd2f3ef76220f35132
lukemesiti <lukemesiti@gmail.com>
2017-07-02 17:21:07 +1000

    add reducer tests

--- src/reducers/jobs__test__.js
+++ src/reducers/jobs__test__.js
@@ -1,5 +1,6 @@
 import expect from 'expect';
 import jobs from './jobs';
+import { SET_JOBS } from '../constants/actions';
 
 describe('jobs reducer ', () => {
     it('Should return the same state if action.type is invalid', () => {
@@ -14,5 +15,30 @@ describe('jobs reducer ', () => {
         const result = jobs(state, action);
         
         expect(result).toEqual(state);
+    });
+
+    it(`Should return an empty array when state is undefined`, () => {
+        const action = {
+            type: 'NOT_AN_ACTION'
+        }
+
+        const result = jobs(undefined, action);
+
+        expect(result).toEqual([]);
+    });
+
+    it(`Should return the action payload when action type is ${SET_JOBS}`, () => {
+        const action = {
+            type: SET_JOBS,
+            payload: [{
+                this: 'is my job',
+                there: 'are many like it',
+                but: 'this one is mine'
+            }]
+        };
+
+        const result = jobs([], action);
+
+        expect(result).toEqual(action.payload);
     });
 });
